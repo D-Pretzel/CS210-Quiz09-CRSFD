3ec1c10958b92734da984e7aaf501d764133cd18
Adrian de Freitas <Adefreit@users.noreply.github.com>
2022-11-15 08:43:31 -0700

    Uploading Templates

---
+++ quiz09-01.c
@@ -0,0 +1,19 @@
+/** quiz09-01.c
+* ===========================================================
+*    Name: 
+* Section:
+* Purpose: Quiz 9
+* ===========================================================
+*/
+
+#include <stdio.h>
+
+// YOUR FUNCTION WILL GO HERE
+
+
+int main(void){
+
+    // We highly recommend that you test your function
+    return 0;
+
+}
---
+++ quiz09-03.c
@@ -0,0 +1,20 @@
+/** quiz09-03.c
+* ===========================================================
+*    Name: 
+* Section:
+* Purpose: Quiz 9
+* ===========================================================
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+// YOUR FUNCTION WILL GO HERE
+
+
+int main(void){
+
+    // We highly recommend that you test your function
+    return 0;
+
+}
